2b6a3edd5352cd4a4187f106dfcc09f41e14c09d
stevekibe <stevekibe2345@gmail.com>
2023-11-19 22:36:53 +0300

    update react

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from "./HomePage";
+import {HomePage} from "./HomePage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import {CreateRoomPage} from "./CreateRoomPage";
 
-export default class App extends Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
     return (
-      <div>
-        <HomePage />
-      </div>
+    <p>This</p>
     );
   }
 }
